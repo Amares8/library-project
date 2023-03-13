e14a11ac7d1873150d12a88dd9869efeca764d06
Amares8 <amadi2003@op.pl>
2023-03-13 10:24:22 +0100

    p1

--- Reszke/Form1.Designer.cs
+++ Reszke/Form1.Designer.cs
@@ -29,15 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.sideNavPanel = new System.Windows.Forms.Panel();
             this.navTimeLabel = new System.Windows.Forms.Label();
             this.navTopImage = new System.Windows.Forms.PictureBox();
-            this.welcomePanel = new System.Windows.Forms.Panel();
             this.navTimeTimer = new System.Windows.Forms.Timer(this.components);
-            this.topTitlePanel = new System.Windows.Forms.Panel();
             this.sideNavPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.navTopImage)).BeginInit();
-            this.welcomePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // sideNavPanel
@@ -54,12 +52,12 @@
             // navTimeLabel
             // 
             this.navTimeLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.navTimeLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.navTimeLabel.Location = new System.Drawing.Point(12, 90);
+            this.navTimeLabel.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.navTimeLabel.Location = new System.Drawing.Point(88, 9);
             this.navTimeLabel.Name = "navTimeLabel";
-            this.navTimeLabel.Size = new System.Drawing.Size(183, 85);
+            this.navTimeLabel.Size = new System.Drawing.Size(123, 68);
             this.navTimeLabel.TabIndex = 1;
-            this.navTimeLabel.Text = "current time displayed\r\nhere";
+            this.navTimeLabel.Text = "current\r\ntime displayed\r\nhere";
             this.navTimeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // navTopImage
@@ -69,51 +67,32 @@
             this.navTopImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.navTopImage.Image = global::Reszke.Properties.Resources.logo;
             this.navTopImage.InitialImage = global::Reszke.Properties.Resources.logo;
-            this.navTopImage.Location = new System.Drawing.Point(55, 12);
+            this.navTopImage.Location = new System.Drawing.Point(3, 0);
             this.navTopImage.Name = "navTopImage";
             this.navTopImage.Size = new System.Drawing.Size(97, 75);
             this.navTopImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.navTopImage.TabIndex = 0;
             this.navTopImage.TabStop = false;
             // 
-            // welcomePanel
-            // 
-            this.welcomePanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.welcomePanel.Controls.Add(this.topTitlePanel);
-            this.welcomePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.welcomePanel.Location = new System.Drawing.Point(211, 0);
-            this.welcomePanel.Name = "welcomePanel";
-            this.welcomePanel.Size = new System.Drawing.Size(899, 574);
-            this.welcomePanel.TabIndex = 1;
-            // 
             // navTimeTimer
             // 
             this.navTimeTimer.Enabled = true;
+            this.navTimeTimer.Interval = 500;
             this.navTimeTimer.Tick += new System.EventHandler(this.navTimeTimer_Tick);
-            // 
-            // topTitlePanel
-            // 
-            this.topTitlePanel.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.topTitlePanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.topTitlePanel.Location = new System.Drawing.Point(0, 0);
-            this.topTitlePanel.Name = "topTitlePanel";
-            this.topTitlePanel.Size = new System.Drawing.Size(899, 100);
-            this.topTitlePanel.TabIndex = 0;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1110, 574);
-            this.Controls.Add(this.welcomePanel);
             this.Controls.Add(this.sideNavPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(350, 250);
             this.Name = "mainForm";
             this.Text = "Biblioteka - Reszke, Zaindin, Wierkin, Wysocki";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.sideNavPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.navTopImage)).EndInit();
-            this.welcomePanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -121,10 +100,8 @@
         #endregion
 
         private Panel sideNavPanel;
-        private Panel welcomePanel;
         private PictureBox navTopImage;
         private Label navTimeLabel;
         private System.Windows.Forms.Timer navTimeTimer;
-        private Panel topTitlePanel;
     }
 }
